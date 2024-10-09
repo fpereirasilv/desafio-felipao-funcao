61e41076d52b78c6cc62c0bffdacaac9563fde3f
fpereirasilv <fpereirasilv@gmail.com>
2024-10-09 14:00:24 -0300

    Correção dos calculos dos rankes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const prompt = require('prompt-sync')({sigint: true});
 let saldoVitorias = 0;
 let nivelHeroi = '';
 
-const pontosVitoria = prompt('Por favor digite o número de vitórias: ');
-const pontosDerrota = prompt('Por favor digite o número de derrotas: ');
+const pontosVitoria = parseInt(prompt('Por favor digite o número de vitórias: '));
+const pontosDerrota = parseInt(prompt('Por favor digite o número de derrotas: '));
 
 function saldoRankeadas(nVitorias, nDerrotas){
     return nVitorias - nDerrotas;
@@ -13,17 +13,17 @@ function saldoRankeadas(nVitorias, nDerrotas){
 saldoVitorias = saldoRankeadas(pontosVitoria, pontosDerrota);
 
 function calculaRanke(ranke){
-    if(ranke < 10) {
+    if(ranke <= 10) {
         return 'Ferro';
-    } else if(ranke > 11 && ranke < 20){
+    } else if(ranke > 11 && ranke <= 20){
         return 'Bronze';
-    } else if(ranke > 21 && ranke < 50){
+    } else if(ranke > 21 && ranke <= 50){
         return 'Prata';
-    } else if(ranke > 51 && ranke < 80){
+    } else if(ranke > 51 && ranke <= 80){
         return 'Ouro';
-    } else if(ranke > 81 && ranke < 90){
+    } else if(ranke > 81 && ranke <= 90){
         return 'Diamante';
-    } else if(ranke > 91 && ranke < 100){
+    } else if(ranke > 91 && ranke <= 100){
         return 'Lendário';
     } else if(ranke >= 101){
         return 'Imortal'
